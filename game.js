148ed7328b079c1901c89de11c4f098b16ac7c99
mseyne <michaelseyne@gmail.com>
2016-09-25 09:54:11 +0200

    first update

---
+++ game.js
@@ -0,0 +1,8 @@
+var game = new ex.Engine({
+    width: 800,
+    height: 600
+});
+var go = 13;
+var a = "hello";
+console.log(go, a);
+game.start();
---
+++ game.ts
@@ -0,0 +1,12 @@
+
+var game = new ex.Engine({
+    width: 800,
+    height: 600
+});
+
+let go:number = 13;
+let a:string = "hello";
+
+console.log(go, a);
+
+game.start();
