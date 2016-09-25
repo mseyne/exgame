f338abfbca3b0db34f2ddea302fe78415faa929a
mseyne <michaelseyne@gmail.com>
2016-09-25 11:04:14 +0200

    add paddle and ball

--- game.js
+++ game.js
@@ -1,8 +1,20 @@
+//game.js
 var game = new ex.Engine({
     width: 800,
     height: 600
 });
-var go = 13;
-var a = "hello";
-console.log(go, a);
+var paddle = new ex.Actor(150, game.getHeight() - 40, 150, 20);
+paddle.color = ex.Color.Azure;
+paddle.collisionType = ex.CollisionType.Fixed;
+game.add(paddle);
+game.input.pointers.primary.on('move', function (evt) {
+    paddle.pos.x = evt.x;
+});
+var ball = new ex.Actor(100, 300, 20, 20);
+ball.color = ex.Color.Red;
+ball.vel.setTo(150, 150);
+ball.collisionType = ex.CollisionType.Elastic;
+game.add(ball);
+ball.on('update', function () {
+});
 game.start();
--- game.ts
+++ game.ts
@@ -1,12 +1,38 @@
+//game.js
 
 var game = new ex.Engine({
     width: 800,
     height: 600
 });
 
-let go:number = 13;
-let a:string = "hello";
 
-console.log(go, a);
+var paddle = new ex.Actor(150, game.getHeight() - 40, 150, 20);
+
+paddle.color = ex.Color.Azure;
+
+paddle.collisionType = ex.CollisionType.Fixed;
+
+game.add(paddle);
+
+game.input.pointers.primary.on('move', function(evt){
+    paddle.pos.x = evt.x;
+});
+
+var ball = new ex.Actor(100, 300, 20, 20);
+
+ball.color = ex.Color.Red;
+
+ball.vel.setTo(150, 150);
+
+ball.collisionType = ex.CollisionType.Elastic;
+
+game.add(ball);
+
+ball.on('update', function() {
+
+});
+
 
 game.start();
+
+console.log(game.currentScene);
