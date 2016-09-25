21229ac980035f5bb3841636d6e77fb42c3e9e47
mseyne <michaelseyne@gmail.com>
2016-09-25 11:33:29 +0200

    add update events

--- game.js
+++ game.js
@@ -15,6 +15,23 @@ ball.color = ex.Color.Red;
 ball.vel.setTo(150, 150);
 ball.collisionType = ex.CollisionType.Elastic;
 game.add(ball);
-ball.on('update', function () {
+ball.on('preupdate', function () {
+    if (this.pos.x < (this.getWidth() / 2)) {
+        this.vel.x *= -1;
+    }
+    if (this.pos.x + (this.getWidth() / 2) > game.getWidth()) {
+        this.vel.x *= -1;
+    }
+    if (this.pos.y - (this.getHeight() / 2) < 0) {
+        this.vel.y *= -1;
+    }
+});
+paddle.on('preupdate', function () {
+    if (this.pos.x < (this.getWidth() / 2)) {
+        paddle.pos.x = this.getWidth() / 2;
+    }
+    if (this.pos.x + (this.getWidth() / 2) > game.getWidth()) {
+        paddle.pos.x = game.getWidth() - (this.getWidth() / 2);
+    }
 });
 game.start();
